ae4a01867e38aa0d1f5063d5cafbe80f0c5d561d
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2023-12-10 00:24:05 +0500

    Add webhook endpoint

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Services\BotService;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TelegramController extends Controller
 {
-    public function webhook(Request $request, BotService $botService): array
+    public function webhook(Request $request, BotService $botService): void
     {
         $botService->run(true);
-
-        return [
-            'success' => true
-        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
-
-Route::get('/telegram/webhook', [TelegramController::class, 'webhook']);
